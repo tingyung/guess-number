6060a6835dc5e1773155df177b56fca77cbf1c6d
Ting <tina19991130@gmail.com>
2021-01-23 22:14:23 +0800

    Add count

--- 猜數字.py
+++ 猜數字.py
@@ -1,13 +1,16 @@
 import random
-
+i = 0
 r = random.randint(1, 100)
 while True:
 	guess = input("請輸入一個數字:")
 	guess = int(guess)
+	i = i + 1
 	if guess == r:
 		print ("猜對了!")
+		print("這是你猜的第" , i, "次")
 		break
-	elif guess >= r:
+	elif guess > r:
 		print ("比答案大")
 	else:
 		print ("比答案小")
+	print("這是你猜的第" , i, "次")
